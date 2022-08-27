6bfc24300e28725ba6672012299ed653cea36fc3
mreid21 <n01437978@unf.edu>
2022-08-27 19:22:43 -0400

    kms

--- api/src/handlers.rs
+++ api/src/handlers.rs
@@ -1,14 +1,54 @@
 use actix_web::{web, get, Responder, HttpResponse};
 use serde::Deserialize;
 
+
 #[derive(Deserialize)]
 pub struct IThemeParkId {
     id: String,
 }
 
+#[allow(dead_code)]
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct WaitTime {
+    id: String,
+    wait_time: Option<u32>,
+    status: Option<String>,
+    active: bool,
+    last_update: Option<String>,
+    name: String,
+    fast_pass: bool,
+    meta: WaitTimeMeta
+}
+
+#[allow(dead_code)]
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct WaitTimeMeta {
+    #[serde(rename(deserialize = "type"))]
+    ride_type: String,
+    longitude: f64,
+    latitude: f64,
+    entity_id: String,
+    single_rider: bool
+}
+
+pub async fn fetch_wait_times(id: String) -> Result<Vec<WaitTime>, reqwest::Error> {
+    let url = format!("https://api.themeparks.wiki/preview/parks/{}/waittime", id);
+
+    let body: Vec<WaitTime> = reqwest::get(url).await?.json().await?;
+    print!("{:?}", body);
+    Ok(body)
+}
+
 #[get("/{id}")]
 pub async fn get_wait_times(data: web::Path<IThemeParkId>) -> impl Responder {
-    HttpResponse::Ok().body(format!("wait times for park {}", data.id))
+
+    let res = fetch_wait_times(data.id.clone()).await;
+    
+    HttpResponse::Ok().body(format!("wait times for attraction {}", data.id))
+    
+
 }
 
 #[get("/attractions/{id}")]
